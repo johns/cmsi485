9667e68f408e476c91f0bbc795990b6f3c878fc4
JohnS <johnrscott4@gmail.com>
2018-09-17 21:57:08 -0700

    Initial commit, fixed some linter issues

---
+++ homework1/SearchTreeNode.py
@@ -0,0 +1,35 @@
+'''
+SearchTreeNodes contain the following information for BFS:
+
+=== state ===
+The state represented by the node, a tuple:
+(x, y) = (col, row)
+
+=== action ===
+The action used to transition from the parent to this node.
+- The action's value is None if the initial state
+- The action's value will be a string "U", "D", "L", "R" otherwise
+
+=== parent ===
+The parent of this node in the search tree.
+- The parent's value is None if the initial state
+- The parent's value is a reference to the parent node otherwise
+
+=== totalCost ===
+The total cost of the path of actions that led to this particular state.
+In the notes, we refer to this value as being evaluated through g(n)
+
+=== heuristicCost ===
+The heuristic estimate of cost to be incurred from this node to the
+optimal solution
+'''
+
+class SearchTreeNode:
+    """TODO"""
+
+    def __init__(self, state, action, parent, total_cost, heuristic_cost):
+        self.state = state
+        self.action = action
+        self.parent = parent
+        self.total_cost = total_cost
+        self.heuristic_cost = heuristic_cost
