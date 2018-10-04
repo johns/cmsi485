05e45a245a17b53c01622967324320dba2e4b766
JohnS <johnrscott4@gmail.com>
2018-10-03 17:16:08 -0700

    Resolved many linter warnings

---
+++ homework2/maze_knowledge_base.py
@@ -0,0 +1,99 @@
+'''
+John Scott
+
+maze_knowledge_base.py
+
+Specifies a simple, Conjunctive Normal Form Propositional
+Logic Knowledge Base for use in Grid Maze pathfinding problems
+with side-information.
+'''
+import unittest
+from maze_clause import MazeClause
+
+
+class MazeKnowledgeBase:
+    """TODO"""
+
+    def __init__(self):
+        self.clauses = set()
+
+    def tell(self, clause):
+        """
+        Adds the given clause to the CNF MazeKnowledgeBase
+        Note: we expect that no clause added this way will ever
+        make the KB inconsistent (you need not check for this)
+        """
+        # TODO: This is currently implemented incorrectly; see
+        # spec for details!
+        return
+
+    def ask(self, query):
+        """
+        Given a MazeClause query, returns True if the KB entails
+        the query, False otherwise
+        """
+        # TODO: Implement resolution inference here!
+        # This is currently implemented incorrectly; see
+        # spec for details!
+        return False
+
+
+class MazeKnowledgeBaseTests(unittest.TestCase):
+    """TODO"""
+    def test_mazekb1(self):
+        """TODO"""
+        kb = MazeKnowledgeBase()
+        kb.tell(MazeClause([(("X", (1, 1)), True)]))
+        self.assertTrue(kb.ask(MazeClause([(("X", (1, 1)), True)])))
+
+    def test_mazekb2(self):
+        """TODO"""
+        kb = MazeKnowledgeBase()
+        kb.tell(MazeClause([(("X", (1, 1)), False)]))
+        kb.tell(MazeClause([(("X", (1, 1)), True), (("Y", (1, 1)), True)]))
+        self.assertTrue(kb.ask(MazeClause([(("Y", (1, 1)), True)])))
+
+    def test_mazekb3(self):
+        """TODO"""
+        kb = MazeKnowledgeBase()
+        kb.tell(MazeClause([(("X", (1, 1)), False), (("Y", (1, 1)), True)]))
+        kb.tell(MazeClause([(("Y", (1, 1)), False), (("Z", (1, 1)), True)]))
+        kb.tell(MazeClause([(("W", (1, 1)), True), (("Z", (1, 1)), False)]))
+        kb.tell(MazeClause([(("X", (1, 1)), True)]))
+        self.assertTrue(kb.ask(MazeClause([(("W", (1, 1)), True)])))
+        self.assertFalse(kb.ask(MazeClause([(("Y", (1, 1)), False)])))
+
+    def test_mazekb4(self):
+        """TODO"""
+        kb = MazeKnowledgeBase()
+        kb.tell(MazeClause([(("X", (1, 1)), False), (("Y", (1, 1)), True), (("W", (1, 1)), True)]))
+        kb.tell(MazeClause([(("W", (1, 1)), False), (("Z", (1, 1)), False), (("S", (1, 1)), True)]))
+        kb.tell(MazeClause([(("S", (1, 1)), False), (("T", (1, 1)), False)]))
+        kb.tell(MazeClause([(("X", (1, 1)), True), (("T", (1, 1)), True)]))
+        kb.tell(MazeClause([(("W", (1, 1)), True)]))
+        kb.tell(MazeClause([(("T", (1, 1)), True)]))
+        self.assertTrue(kb.ask(MazeClause([(("Z", (1, 1)), False)])))
+
+    def test_mazekb5(self):
+        """TODO"""
+        kb = MazeKnowledgeBase()
+        kb.tell(MazeClause([(("X", (1, 1)), False), (("Y", (1, 1)), True), (("W", (1, 1)), True)]))
+        kb.tell(MazeClause([(("W", (1, 1)), False), (("Z", (1, 1)), False), (("S", (1, 1)), True)]))
+        kb.tell(MazeClause([(("S", (1, 1)), False), (("T", (1, 1)), False)]))
+        kb.tell(MazeClause([(("X", (1, 1)), True), (("T", (1, 1)), True)]))
+        kb.tell(MazeClause([(("W", (1, 1)), True)]))
+        kb.tell(MazeClause([(("T", (1, 1)), True)]))
+        self.assertTrue(kb.ask(MazeClause([(("Z", (1, 1)), True), (("W", (1, 1)), True)])))
+
+    def test_mazekb6(self):
+        """TODO"""
+        kb = MazeKnowledgeBase()
+        kb.tell(MazeClause([(("X", (1, 1)), False), (("Y", (1, 1)), False), (("Z", (1, 1)), False)]))
+        kb.tell(MazeClause([(("X", (1, 1)), True)]))
+        self.assertFalse(kb.ask(MazeClause([(("Z", (1, 1)), False)])))
+        kb.tell(MazeClause([(("Y", (1, 1)), True)]))
+        self.assertTrue(kb.ask(MazeClause([(("Z", (1, 1)), False)])))
+
+
+if __name__ == "__main__":
+    unittest.main()
