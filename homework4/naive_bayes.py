30894475507ad8b1451b86c1a15c267c4b6e1f7c
Jmyerzzz <jacksontmyers@gmail.com>
2018-12-09 23:04:26 -0800

    need to figure out how to test efficacy of model

--- homework4/naive_bayes.py
+++ homework4/naive_bayes.py
@@ -12,39 +12,47 @@ import numpy as np
 import pandas as pd
 import scipy
 from sklearn import preprocessing
-from sklearn.naive_bayes import BernoulliNB
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.impute import SimpleImputer
 
 
 class NaiveBayesClassifier:
-    """
 
-    """
-    #np.set_printoptions(threshold=np.nan)
-    names = ("age", "wrk_cls", "educ", "educ_num", "marital_sts", "occu_code", "realtion", "race", "sex", "gain", "loss", "hours", "country", "income")
+    def preprocess(file_name):
+        names = ("age", "wrk_cls", "educ", "educ_num", "marital_sts", "occu_code", "realtion", "race", "sex", "gain", "loss", "hours", "country", "income")
 
+        original_data = pd.read_csv(file_name, dtype=object, names=names)
+        original_data = pd.DataFrame(original_data)
 
-    original_data = pd.read_csv("income-training.csv", dtype=object, names=names)
-    original_data = pd.DataFrame(original_data)
+        imp = SimpleImputer(missing_values=' ?', strategy='most_frequent', verbose=1)
+        data = imp.fit_transform(original_data)
 
-    imp = SimpleImputer(missing_values=' ?', strategy='most_frequent', verbose=1)
-    data = imp.fit_transform(original_data)
+        enc = preprocessing.OrdinalEncoder()
+        data = enc.fit_transform(data)
 
-    enc = preprocessing.OneHotEncoder()
-    data = enc.fit_transform(data)
-    print(data.toarray())
+        est = preprocessing.KBinsDiscretizer(encode="ordinal")
+        data = est.fit_transform(data)
 
-    classifier = BernoulliNB()
-    classifier.fit(data, income-test.csv)
+        return data
 
+    def train_model(data):
+        X, Y = data[:, :-1], data[:, -1]
+        print(X)
+        print(Y)
 
-    def __init__(self):
-        """
+        classifier = MultinomialNB()
+        classifier.fit(X, Y)
+        return classifier
 
-        """
 
 class NaiveBayesClassifierTests(unittest.TestCase):
-    """Unit Tests"""
+    def test(self):
+
+        processed_training_data = NaiveBayesClassifier.preprocess("income-training.csv")
+        processed_test_data = NaiveBayesClassifier.preprocess("income-test.csv")
+
+        trained_model = NaiveBayesClassifier.train_model(processed_training_data)
+        print(trained_model.predict(processed_test_data))
 
 
 if __name__ == "__main__":
