3ac39136857f599dab9c40818ec7debc41148b7c
Ben Smith <ben.protus.smith@gmail.com>
2018-12-10 20:45:46 -0800

    Cleaned up code

--- homework4/naive_bayes.py
+++ homework4/naive_bayes.py
@@ -9,9 +9,7 @@ naive_bayes.py
 
 import unittest
 import warnings
-import numpy as np
 import pandas as pd
-import scipy
 from sklearn import preprocessing
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.impute import SimpleImputer
@@ -31,40 +29,28 @@ class NaiveBayesClassifier:
         """
         names = ("age", "wrk_cls", "edu", "edu_num", "marital_sts", "occu_code",
                  "relation", "race", "sex", "gain", "loss", "hours", "country", "income")
-
         original_data = pd.read_csv(file_name, dtype=object, names=names)
         original_data = pd.DataFrame(original_data)
-
         imp = SimpleImputer(missing_values=' ?', strategy='most_frequent', verbose=1)
         data = imp.fit_transform(original_data)
-
         enc = preprocessing.OrdinalEncoder()
         data = enc.fit_transform(data)
-
         est = preprocessing.KBinsDiscretizer(encode="ordinal")
         data = est.fit_transform(data)
-
         return data
 
     def train_model(data):
-        """
-        Trains agent on preprocessed data.
-        """
-        X, Y = data[:, :-1], data[:, -1]
-
+        """ Trains agent on preprocessed data """
         classifier = MultinomialNB()
+        X, Y = data[:, :-1], data[:, -1]
         classifier.fit(X, Y)
         return classifier
 
 
 class NaiveBayesClassifierTests(unittest.TestCase):
-    """
-    Tests to determine the efficacy of our agent.
-    """
+    """ Tests to determine the efficacy of our agent """
     def setUp(self):
-        """
-        Suppresses annoying warning
-        """
+        """ Suppresses annoying warning """
         warnings.simplefilter('ignore', category=ImportWarning)
 
     def test(self):
@@ -73,7 +59,6 @@ class NaiveBayesClassifierTests(unittest.TestCase):
         """
         processed_training_data = NaiveBayesClassifier.preprocess("income-training.csv")
         processed_test_data = NaiveBayesClassifier.preprocess("income-test.csv")
-
         trained_model = NaiveBayesClassifier.train_model(processed_training_data)
         accuracy = trained_model.score(processed_test_data[:, :-1], processed_test_data[:, -1])
         print("Agent Prediction Accuracy: ", round(accuracy * 100, 2), "%")
